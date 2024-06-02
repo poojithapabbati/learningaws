a3a3f705c82b5e9046d0790243b36df64a6db772
pooji <pabbathi.pooji99@gmail.com>
2024-06-02 18:15:51 +0530

    final commit

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,8 @@ provider "aws" {
 }
 resource "aws_instance" "app-server" {
   instance_type="t2.micro"
-  key_name = "pj2"
+  ami = "ami-0f142a8c1a3484f69"
+  key_name = ""pj2"
   monitoring = true
   vpc_security_group_ids = [ "sg-08c0e8649d7b3efb9" ]
   subnet_id = "subnet-09334403cb3648032"
